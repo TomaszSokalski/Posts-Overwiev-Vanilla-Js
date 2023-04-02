3b692e1a585ee02ca06e3015c7dbc26c53bad619
TomaszSokalski <tsokalski88@gmail.com>
2023-04-02 02:27:29 +0200

    add footer

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 import { navigation } from './Navigation';
-import { posts } from './Posts'
+import { posts } from './Posts';
+import { footer } from './Footer';
 
 window.onload = () => {
     navigation.init();
     posts.init();
+    footer.init();
 }
