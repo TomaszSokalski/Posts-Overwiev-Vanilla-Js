52f5bcee186840297b68ecd38eec9e80999d86bf
TomaszSokalski <tsokalski88@gmail.com>
2023-04-03 14:35:08 +0200

    render new post after POST method

--- src/js/Navigation.js
+++ src/js/Navigation.js
@@ -1,7 +1,9 @@
-import {NAVIGATION, DIALOG_FORM} from './elements';
+import {NAVIGATION, DIALOG_FORM, POSTS} from './elements';
 import { postsService } from './PostsService'
 
 class Navigation  {
+    payLoad = {};
+
     init() {
         this._render(NAVIGATION);
         this._addTask();
@@ -24,7 +26,6 @@ class Navigation  {
     }
 
     _addTask() {
-        const payLoad = {};
         NAVIGATION.addEventListener('click', e => {
             if (e.target.classList.contains('nav-btn')) {
                 DIALOG_FORM.showModal();
@@ -34,8 +35,7 @@ class Navigation  {
                 DIALOG_FORM.innerHTML = `
                 <form class="form__data">
                     <h1>Add post</h1>
-                    <input type="text" name="title" placeholder="Post title" required>
-                    <textarea type="text" name="body" placeholder="Post description" required></textarea>
+                    <textarea type="text" name="title" placeholder="Post description" required></textarea>
                     <div><button type="submit" class="btn btn__primary">Add</button></div>
                     <div><button type="button" class="btn btn-close btn__warn">Close</button></div>
                 </form>
@@ -47,9 +47,31 @@ class Navigation  {
 
 
                     for (const [key, value] of formData) {
-                        payLoad[key] = value
+                        this.payLoad[key] = value;
                     }
-                    postsService.create(payLoad);
+                    const postData = async () => {
+                        this.payLoad = await postsService.create(this.payLoad);
+
+                        let postContent = '';
+
+                        //lanugage=HTML
+                        postContent += `
+                        <div class="card__container">
+                            <h5>Post no.<span>${this.payLoad.id}</span></h5>
+                            <p>${this.payLoad.title}</p>
+                            <button class="btn btn-details btn__primary ">Show description</button>
+                            <button class="btn btn-delete btn__warn ">Delete <i class="p-icon--delete"></i></button>
+                        </div>
+                        `;
+
+                        const newPost = document.createElement('div');
+                        newPost.classList.add('card');
+                        newPost.innerHTML = postContent;
+
+                        POSTS.appendChild(newPost);
+                    }
+
+                    postData();
                     DIALOG_FORM.close();
                     FORM.reset();
                 });
