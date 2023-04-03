8e6637c6c15574ab50fbbb88a62b120394e8c0bd
TomaszSokalski <tsokalski88@gmail.com>
2023-04-03 14:02:29 +0200

    add description dialog

--- src/js/elements.js
+++ src/js/elements.js
@@ -4,4 +4,5 @@ export const getElements = selector => document.querySelectorAll(selector);
 export const NAVIGATION = getElement('nav');
 export const POSTS = getElement('section');
 export const FOOTER = getElement('footer');
-export const DIALOG = getElement('dialog');
+export const DIALOG_FORM = getElement('.form');
+export const DIALOG_DETAILS = getElement('.details');
